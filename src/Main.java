a0c6bef01594d00727078a3f15af3126dcf72806
Valeriia Radzivilo <v.radzivilo.ip14@kpi.ua>
2024-05-08 20:24:38 +0300

    - Code cleaning

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,42 @@ import mpi.MPI;
 import mpi.MPIException;
 
 public class Main {
+
+    static final boolean PRINT_MATRICES = false;
+
     public static void main(String[] args) throws MPIException {
         MPI.Init(args);
 
         int rank = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
 
-        int N = 4; // Розмір матриці
+        int N = 2000;
 
+        if (N % size != 0) {
+            throw new IllegalArgumentException("Matrix size should be divisible by number of processors");
+        }
 
-        // Ініціалізація матриць
 
         Matrix A = Matrix.generateRandom(N, N);
         Matrix B = Matrix.generateRandom(N, N);
         Matrix C = new Matrix(N, N);
 
-        if (rank == 0) {
+        if (rank == 0 && PRINT_MATRICES) {
             System.out.println("Matrix A:");
             A.print();
             System.out.println("Matrix B:");
             B.print();
         }
 
-        // Розподіл матриці між процесами
+
         MPI.COMM_WORLD.Bcast(A.matrix, 0, N, MPI.OBJECT, 0);
         MPI.COMM_WORLD.Bcast(B.matrix, 0, N, MPI.OBJECT, 0);
 
-        // Обчислення діапазону для кожного процесу
+
         int from = rank * N / size;
         int to = (rank + 1) * N / size;
 
-        // Множення матриць
+
         for (int i = from; i < to; i++) {
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < N; k++) {
@@ -41,14 +46,15 @@ public class Main {
             }
         }
 
-        // Збір результатів
+
         MPI.COMM_WORLD.Gather(C.matrix, from, to - from, MPI.OBJECT, C.matrix, 0, to - from, MPI.OBJECT, 0);
 
-        // print result
-        if (rank == 0) {
-            System.out.println("Result:");
-            C.print();
 
+        if (rank == 0) {
+            if (PRINT_MATRICES) {
+                System.out.println("Result:");
+                C.print();
+            }
             // Check result
             Matrix expected = A.multiply(B);
             if (C.equals(expected)) {
